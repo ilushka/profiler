727082ab8653f4c7ec821c2a48464d6b964aace2
root <root@ip-10-16-24-243.ec2.internal>
2012-09-25 18:51:22 +0000

    -- added support for dual GPU usage reporting. -- added headers to the CSV file.

--- profile.rb
+++ profile.rb
@@ -3,14 +3,24 @@
 require 'optparse'
 
 csvfile = nil
+dualgpu = false
 
 opt_parse = OptionParser.new do |opt_parse|
   opt_parse.banner = "Usage: profile.rb [options]"
   opt_parse.separator ""
   opt_parse.separator "Options:"
 
+  opt_parse.on("--dualgpu", "Collect usage from two GPUs.") do |arg|
+    dualgpu = true
+  end
+
   opt_parse.on("--csvfile FILENAME", "Save CPU, GPU, GPU memory usage in CSV format to FILENAME.") do |csvfilename|
     csvfile = File.open(csvfilename, 'w')
+    if dualgpu
+      csvfile.write("CPU%,GPU0%,GPU0 Mem%,GPU1%,GPU1 Mem%,\n")
+    else
+      csvfile.write("CPU%,GPU%,GPU Mem%,\n")
+    end
   end
 
   opt_parse.on_tail("--help", "Show this message.") do
@@ -20,6 +30,30 @@ opt_parse = OptionParser.new do |opt_parse|
 end # opt_parse = OptionParse new do |opt_parse|
 
 opt_parse.parse!(ARGV)
+
+def parse_single_gpu_usage(output, csvfile)
+  output.scan(/.*Gpu\s+\:\s(\d+)\s\%.*Memory\s+\:\s(\d+)\s\%.*/m) do |gpu, mem|
+    puts "GPU% - #{gpu}"
+    puts "GPU MEM% - #{mem}"
+
+    if !csvfile.nil?
+      csvfile.write("#{gpu},#{mem},\n")
+    end
+  end
+end
+
+def parse_dual_gpu_usage(output, csvfile)
+  output.scan(/.*Gpu\s+\:\s(\d+)\s\%.*Memory\s+\:\s(\d+)\s\%.*Gpu\s+\:\s(\d+)\s\%.*Memory\s+\:\s(\d+)\s\%.*/m) do |gpu0, mem0, gpu1, mem1|
+    puts "GPU0% - #{gpu0}"
+    puts "GPU0 MEM% - #{mem0}"
+    puts "GPU1% - #{gpu1}"
+    puts "GPU1 MEM% - #{mem1}"
+
+    if !csvfile.nil?
+      csvfile.write("#{gpu0},#{mem0},#{gpu1},#{mem1},\n")
+    end
+  end
+end
 
 File.open('/proc/stat', 'r') do |stat|
   stat.seek(0, IO::SEEK_SET)
@@ -51,13 +85,10 @@ File.open('/proc/stat', 'r') do |stat|
       cpu_total_old = cpu_total_new
       cpu_idle_old = cpu_idle_new
 
-      nvidia_output.scan(/.*Gpu\s+\:\s(\d+)\s\%.*Memory\s+\:\s(\d+)\s\%.*/m) do |gpu, mem|
-        puts "GPU% - #{gpu}"
-        puts "GPU MEM% - #{mem}"
-
-        if !csvfile.nil?
-          csvfile.write("#{gpu},#{mem},\n")
-        end
+      if dualgpu
+        parse_dual_gpu_usage(nvidia_output, csvfile)
+      else
+        parse_single_gpu_usage(nvidia_output, csvfile)
       end
 
       puts "--"
